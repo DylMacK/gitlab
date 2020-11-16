385c6471bc4021b94cc62cfde477f19d1c4c03e0
Dylan <dmmackenzie4@gmail.com>
2020-11-16 11:05:13 -0500

    Added lost public static void main

--- Hello.java
+++ Hello.java
@@ -5,21 +5,15 @@ package hello;
  *
  * @author Dylan
  */
-public class Hello 
-
+public class Hello
 {
-
+ public static void main( String argv[] )
+ {
  String name = "World";
  if ( argv.length != 0 )
-
  {
-
  name = argv[0];
-
  }
-
  System.out.println( "Hello, " + name + "!" );
-
  }
-
 }
